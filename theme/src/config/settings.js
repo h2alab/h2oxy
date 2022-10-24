8062468668fc719d36b0730a927d43269a3a863c
Henrique Meira <henrique@meira.net>
2022-10-24 22:28:20 +0000

    libera temas

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -3,6 +3,6 @@ export default {
 	description: `O combate definitivo aos vírus e bactérias.`,
 	url: `https://h2oxy.netlify.app`, // No trailing slash!
 	name: `H2Oxy`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: false,
+  enableThemeSwitcher: true,
   showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
